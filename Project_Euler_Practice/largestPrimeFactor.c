b93357bc16691634f29f3e5e99474b209351244e
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-07-11 16:16:10 -0500

    solution works with smaller number

--- Project_Euler_Practice/largestPrimeFactor.c
+++ Project_Euler_Practice/largestPrimeFactor.c
@@ -9,9 +9,38 @@ PROBLEM:
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 
+NOTES:
+
+- prime # is a # that is only divisible by 1 and itself
+
+steps:
+
+- find a prime factor
+- divide num by prime factor
+
 */
 
-int main(int argc, int *argv[])
+int main(void)
 {
-    
+    int num = 460;
+    int largestPrimeFactor = 0;
+    int i = 2;
+
+    while (num / i >= 1)
+    {
+        if (num % i == 0)
+        {
+            if (i > largestPrimeFactor)
+            {
+                largestPrimeFactor = i;
+                printf("** current largest prime factor is %d\n", largestPrimeFactor);
+            }
+            num = num / i;
+            printf("current number is: %d\n", num);
+            i = 1;
+        }
+        i++;
+    }
+    printf("%d\n", largestPrimeFactor);
+    return 0;
 }
