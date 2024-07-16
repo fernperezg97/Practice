e7426487db776d9679090f0c7ab37615ce0234ed
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-07-16 16:35:18 -0500

    have a good start on problem and it compiles

--- Project_Euler_Practice/smallestMultiple.c
+++ Project_Euler_Practice/smallestMultiple.c
@@ -1,5 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 /*
@@ -11,6 +12,32 @@ What is the smallest positive number that is evenly divisible with no remainderb
 
 NOTES:
 
-
+- variable to track current largest num
+- num % 1-20 has to = 0
+- if smallest num that can be divided by 1-10 = 2520 does that mean that should be starting point?
+- while loop since we don't have end point
 
 */
+
+bool isDivisibleBy(int num)
+{
+    for (int i = 1; i <= 20; i++) {
+        if (num % i != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(void)
+{
+    int n = 2520;
+    while (true) {
+        if (isDivisibleByAll(n)) {
+            printf("The smallest number divisible by all numbers from 1 to 20 is %d\n", n);
+            break;
+        }
+        n++;
+    }
+    return 0;
+}
