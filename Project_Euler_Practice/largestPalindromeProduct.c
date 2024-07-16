dbbe1f0711c8ec35117848ff73c0d79dadccc145
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-07-16 13:12:50 -0500

    added functions to reverse num and count number of digits

--- Project_Euler_Practice/largestPalindromeProduct.c
+++ Project_Euler_Practice/largestPalindromeProduct.c
@@ -18,6 +18,9 @@ steps:
 - while loop since we don't know the ending result
 - way to figure out if num is a palindrome [::-1] in Python allows you to read the items 'backwards' to check
 
+while num * num != reverseNum(num x num) XXXXX --> NOPE! Doesn't work on 2nd thought bc could have num that isn't LARGEST be a palindrome.
+
+
 */
 
 int reverseNum(int num)
@@ -32,7 +35,27 @@ int reverseNum(int num)
     return reverse;
 }
 
+int numDigits(int n)
+{
+    int n, count = 0;
+    
+    // iterate at least once, then until n becomes 0
+    // remove last digit from n in each iteration
+    // increase count by 1 each iteration
+
+    do {
+        n /= 10;
+        ++count;
+    } while (n != 0);
+
+    return count;
+}
+
 int main()
 {
-    
+    int num1 = 100, num2 = 100, product = num1 * num2;
+    while (product != reverseNum(product))
+    {
+
+    }
 }
