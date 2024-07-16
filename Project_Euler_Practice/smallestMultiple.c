0671206871f2b0f6003a04871f524bab10431edf
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-07-16 16:17:06 -0500

    initial commit for smallestMultiple problem

---
+++ Project_Euler_Practice/smallestMultiple.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+
+PROBLEM:
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible with no remainderby all of the numbers from 1 to 20?
+
+NOTES:
+
+
+
+*/
