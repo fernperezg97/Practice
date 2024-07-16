0a6938016fb59859686584dfb95905c5657565c2
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-07-16 12:42:38 -0500

    initial commit largest palindrome problem

---
+++ Project_Euler_Practice/largestPalindromeProduct.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+
+PROBLEM:
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+NOTES:
+
+steps:
+
+- two variables to keep track of num1 and num2
+- variable to keep track of product
+- while loop since we don't know the ending result
+- way to figure out if num is a palindrome [::-1] in Python allows you to read the items 'backwards' to check
+
+*/
+
