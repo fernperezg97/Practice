dd6f2a099a409747d800469234164fc08e87d652
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-07-16 16:14:12 -0500

    problem solved after fixing small errors

--- Project_Euler_Practice/largestPalindromeProduct.c
+++ Project_Euler_Practice/largestPalindromeProduct.c
@@ -19,39 +19,36 @@ steps:
 - way to figure out if num is a palindrome [::-1] in Python allows you to read the items 'backwards' to check
 
 while num * num != reverseNum(num x num) XXXXX --> NOPE! Doesn't work on 2nd thought bc could have num that isn't LARGEST be a palindrome.
-
+- go from large to small nums ex. 999 x 998 vs 100 X 101
 
 */
 
 int reverseNum(int num)
 {
-    int n, reverse = 0, remainder;
-    while (n!= 0)
+    int reverse = 0, remainder;
+    while (num!= 0)
     {
-        remainder = n % 10;
+        remainder = num % 10;
         reverse = reverse * 10 + remainder;
-        n /= 10;
+        num /= 10;
     }
     return reverse;
 }
 
 int main()
 {
-    int num1 = 100, num2 = 100, product = 0, currentLargesProduct = 0;
+    int product, currentLargestProduct = 0;
 
-    // check product of nums while nums are less than 4 digits
-    while (num1 < 999 && num2 < 999)
-    {   
-        product = num1 * num2;
-        if (product > currentLargesProduct)
+    for (int num1 = 999; num1 >= 100; num1--)
+    {
+        for (int num2 = 999; num2 >= 100; num2--)
         {
-            currentLargesProduct = product;
-            if (currentLargesProduct == reverseNum(currentLargesProduct))
+            product = num1 * num2;
+            if (product == reverseNum(product) && product > currentLargestProduct)
             {
-                printf("%d\n", currentLargesProduct);
+                currentLargestProduct = product; 
             }
-        }
-        num1++;
-        num2++;
+        }  
     }
+    printf("%d\n", currentLargestProduct);     
 }
